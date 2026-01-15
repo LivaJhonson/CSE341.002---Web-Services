e4872fb3c579775bb3c2870072d6729554703bfa
LivaJhonson <livajhonson69@gmail.com>
2026-01-15 17:03:56 +0300

    Complete Week 02: Added CRUD operations and Swagger documentation

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Contacts API'
+  },
+  host: 'cse341-002-web-services-ue3n.onrender.com', // Put your Render URL here later
+  schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
